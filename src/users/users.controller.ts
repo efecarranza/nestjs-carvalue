2063d5fe529ccc6c404cf4b8671ee6c06abb07f2
piscu Carranza <fcarr004@fiu.edu>
2022-02-13 20:36:12 -0300

    interceptors

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,25 @@
-import { Body, Controller, Delete, Get, Patch, Param, Query, Post, NotFoundException } from '@nestjs/common';
+import { 
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Patch,
+	Param,
+	Query,
+	Post,
+	NotFoundException
+} from '@nestjs/common';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
 	constructor(private usersService: UsersService) {}
+
 	@Post('/signup')
 	createUser(@Body() body: CreateUserDto) {
 		this.usersService.create(body.email, body.password);
